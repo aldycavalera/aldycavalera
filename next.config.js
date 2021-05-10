7084164ca00e49afdb080e90a67e61f7ea96263c
aldycavalera <personal.cavalera@gmail.com>
2021-05-10 11:32:20 +0700

    refix

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = withSourceMaps({
     MAILCHIMP_API_KEY: '55fdf82d1a88f64f51d989ea8a514173-us7',
     CONTENTFUL_SPACE_ID: 'g8fljn7062er',
     CONTENTFUL_ACCESS_TOKEN: 'O0IoLgm3bh1-8iQLou87eKc34uSL-qYlqYuNxn1rzb8',
-    API_URL: 'https://aldycavalera.vercel.app',
+    API_URL: 'https://api-aldycavalera.vercel.app',
     // API_URL: 'http://aldycavalera.vercel.app',
     DEFAULT_LOCALE: 'id-ID'
   },
